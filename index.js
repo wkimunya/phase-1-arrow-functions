53e8a9125a7be81d07b0abf5bf6e8a07cc9a85f9
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-10 19:22:44 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide() {
+    console.log(num1/num2);
+    return num1/num2;
+}
+
+function divide() {
+    console.log(2000/100);
+    return 2000/100;
+}
+
+let square = number => 4;
+
+let add = number => 7;
